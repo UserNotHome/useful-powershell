b7b279099e8dd7351531a86a5f44245a78584a47
UserNotHome <wallharrison@protonmail.com>
2024-03-24 17:54:04 +0000

    Who knew you couldn't user $input as an input

--- incident-response/CredentialLeakAccountActive.ps1
+++ incident-response/CredentialLeakAccountActive.ps1
@@ -1,27 +1,26 @@
 # Accept input from the command line
 param(
-    [Parameter(Mandatory=$true)]
-    [string]$input
+    [Parameter(Mandatory=$True)]
+    [string]$account
 )
 
 # Import the Active Directory module
 Import-Module ActiveDirectory
 
-function check {
-    param(
-        [string]$userPrincipalName
-    )
-    # Collect account details
-    $user = Get-ADUser -Filter {UserPrincipalName -eq $userPrincipalName} -Properties Title, Department, LastLogonDate, pwdLastSet, Enabled
-
-    # If account is enabled, return properties
+function accountCheck($UPN) {
+    # Get the account properties
+    $user = Get-ADUser -Filter {UserPrincipalName -eq $UPN} -Properties UserPrincipalName, Title, Department, LastLogonDate, passwordLastSet, Enabled
+    
+    # Us the account valid
     if ($user -ne $null) {
+        # Check if the account is enabled
         if ($user.Enabled -eq $true) {
-            Write-Host "Account Status: Enabled"
+            Write-Host "Account: $($user.UserPrincipalName)"
             Write-Host "Title: $($user.Title)"
             Write-Host "Department: $($user.Department)"
             Write-Host "Last Logon Date: $($user.LastLogonDate)"
-            Write-Host "Password Last Set: $(Get-Date $user.pwdLastSet -Format 'dd-MM-yyyy HH:mm:ss')"
+            Write-Host "Password Last Set: $(Get-Date $user.passwordLastSet -Format 'dd-MM-yyyy HH:mm:ss')"
+            Write-Host "Account Status: Enabled"
         } else {
             Write-Host "The account is disabled."
         }
@@ -30,10 +29,19 @@ function check {
     }
 }
 
-if (Test-Path $input) {
-    Get-Content $input | ForEach-Object {
-        check -userPrincipalName $_
+# Check if the $account is not empty
+if (-not [string]::IsNullOrWhiteSpace($account)) {
+    # Determine if the input is a file path or a UPN
+    if (Test-Path $account) {
+        # If it's a file, read UPNs line by line
+        $UPNs = Get-Content -Path $account
+        foreach ($UPN in $UPNs) {
+            accountCheck $UPN
+        }
+    } else {
+        # If it's not a file, assume it is a single UPN
+        accountCheck $account
     }
 } else {
-    check -userPrincipalName $input
-}
+    Write-Host "Please provide a UserPrincipalName or a path to a file containing UserPrincipalNames."
+} 
