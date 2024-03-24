36903fcb8d55c5f18b2f849c10e2627b0b213f1b
UserNotHome <wallharrison@protonmail.com>
2024-03-24 17:12:01 +0000

    Add functionality to accept account OR file

--- incident-response/CredentialLeakAccountActive.ps1
+++ incident-response/CredentialLeakAccountActive.ps1
@@ -1,27 +1,39 @@
-# Accept UserPrincipalName from the command line argument
+# Accept input from the command line
 param(
     [Parameter(Mandatory=$true)]
-    [string]$userPrincipalName
+    [string]$input
 )
 
 # Import the Active Directory module
 Import-Module ActiveDirectory
 
-# Get the user account details including additional properties
-$user = Get-ADUser -Filter {UserPrincipalName -eq $userPrincipalName} -Properties Title, Department, LastLogonDate, pwdLastSet, Enabled
+function check {
+    param(
+        [string]$userPrincipalName
+    )
+    # Collect account details
+    $user = Get-ADUser -Filter {UserPrincipalName -eq $userPrincipalName} -Properties Title, Department, LastLogonDate, pwdLastSet, Enabled
 
-# Check if the account is found
-if ($user -ne $null) {
-    # Check if the account is enabled
-    if ($user.Enabled -eq $true) {
-        Write-Host "Account Status: Enabled"
-        Write-Host "Title: $($user.Title)"
-        Write-Host "Department: $($user.Department)"
-        Write-Host "Last Logon Date: $($user.LastLogonDate)"
-        Write-Host "Password Last Set: $(Get-Date $user.pwdLastSet -Format 'dd-MM-yyyy HH:mm:ss')"
+    # If account is enabled, return properties
+    if ($user -ne $null) {
+        if ($user.Enabled -eq $true) {
+            Write-Host "Account Status: Enabled"
+            Write-Host "Title: $($user.Title)"
+            Write-Host "Department: $($user.Department)"
+            Write-Host "Last Logon Date: $($user.LastLogonDate)"
+            Write-Host "Password Last Set: $(Get-Date $user.pwdLastSet -Format 'dd-MM-yyyy HH:mm:ss')"
+        } else {
+            Write-Host "The account is disabled."
+        }
     } else {
-        Write-Host "The account is disabled."
+        Write-Host "The account was not found."
+    }
+}
+
+if (Test-Path $input) {
+    Get-Content $input | ForEach-Object {
+        check -userPrincipalName $_
     }
 } else {
-    Write-Host "The account was not found."
+    check -userPrincipalName $input
 }
